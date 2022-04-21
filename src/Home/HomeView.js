62d776db4d7a23c7b2f5607e1900f0ce55f6631c
kayllan <kayllan@kayllans-MBP.Dlink>
2022-04-21 14:29:49 +0200

    sqlite implementation

--- src/Home/HomeView.js
+++ src/Home/HomeView.js
@@ -1,11 +1,12 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import {View, SafeAreaView} from 'react-native';
 import moment from 'moment';
 import {Button} from '@react-native-material/core';
 import {Text} from 'react-native-paper';
+import {openDatabase} from 'react-native-sqlite-storage';
 
 import HomeViewStyles from './HomeViewStyles';
-
+let db = openDatabase({name: 'CollectAll.db'});
 const HomeView = props => {
   const {navigation} = props;
   const {DateContainer, DateTextStyle, SubView, ButtonView, ButtonStyle} =
@@ -13,16 +14,76 @@ const HomeView = props => {
   const TodaysDate = moment().format('ddd DD MMMM YYYY');
   const greeting = 'Hello DRIVER';
   const listOfJobs = 'There are 0 job cards scheduled for today';
+  const [driver, setDriver] = useState();
+  const [jobsLenght, setJobsLenght] = useState();
+
+  const openDB = () => {
+    db = openDatabase(
+      {
+        name: 'CollectAll.db',
+        createFromLocation: 1,
+      },
+      success, //okCallback
+      fail, // error callback
+    );
+  };
+
+  const success = () => {
+    getDriverDetail();
+    getNumOfJobs();
+  };
+
+  const fail = error => {
+    console.error(error); // logging out error if there is one
+  };
+
+  const getDriverDetail = () => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'SELECT * FROM Driver_Table',
+        [],
+        (tx, results) => {
+          var temp = [];
+          for (let i = 0; i < results.rows.length; ++i)
+            temp.push(results.rows.item(i));
+
+          setDriver(temp[0]);
+        },
+        error => {
+          console.log(error);
+        },
+      );
+    });
+  };
+
+  const getNumOfJobs = () => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'SELECT * FROM Jobs_Table',
+        [],
+        (tx, results) => {
+          setJobsLenght(results.rows.length);
+        },
+        error => {
+          console.log(error);
+        },
+      );
+    });
+  };
+
   return (
     <SafeAreaView>
+      {openDB()}
       <View style={DateContainer}>
         <Text style={DateTextStyle}>{TodaysDate}</Text>
       </View>
       <View style={SubView}>
-        <Text style={{color: '#000'}}>{greeting}</Text>
+        <Text style={{color: '#000'}}>Hello {driver?.driver_name}</Text>
       </View>
       <View style={SubView}>
-        <Text style={{color: '#000'}}>{listOfJobs}</Text>
+        <Text style={{color: '#000'}}>
+          There are {jobsLenght} job cards scheduled for today
+        </Text>
       </View>
       <View style={ButtonView}>
         <Button
--- src/Pin/EnterPinView.js
+++ src/Pin/EnterPinView.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import {SafeAreaView, View, TouchableOpacity} from 'react-native';
-import {Title, Button, Text} from 'react-native-paper';
+import {Title, Text} from 'react-native-paper';
+import {openDatabase} from 'react-native-sqlite-storage';
+import NetInfo from '@react-native-community/netinfo';
 
 import Delete from '../assets/delete.svg';
 import {getLocation} from '../Utils/Utilities';
@@ -20,7 +22,291 @@ const EnterPinView = props => {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    console.log(count);
+    const unsubscribe = NetInfo.addEventListener(state => {
+      console.log('Connection type', state.type);
+      console.log('Is connected?', state.isConnected);
+    });
+
+    // Unsubscribe
+    unsubscribe();
+  }, []);
+  let db = null;
+  db = openDatabase({name: 'CollectAll.db'});
+
+  const createDB = () => {
+    db = openDatabase(
+      {
+        name: 'CollectAll.db',
+        createFromLocation: 1,
+      },
+      success, //okCallback
+      fail, // error callback
+    );
+  };
+
+  const success = () => {
+    createTables();
+  };
+
+  const fail = error => {
+    console.error(error); // logging out error if there is one
+  };
+
+  const createTables = () => {
+    db.transaction(function (txn) {
+      //Create driver table
+      txn.executeSql(
+        "SELECT * FROM sqlite_master WHERE type='table' AND name='Driver_Table'",
+        [],
+        function (tx, res) {
+          // console.log('Driver_Table item:', res.rows.length);
+          if (res.rows.length == 0) {
+            txn.executeSql('DROP TABLE IF EXISTS Driver_Table', []);
+            txn.executeSql(
+              'CREATE TABLE IF NOT EXISTS Driver_Table(driver_id INTEGER PRIMARY KEY AUTOINCREMENT, driver_name VARCHAR(30), driver_phone VARCHAR(15), driver_address VARCHAR(255), driver_pin VARCHAR(4))',
+              [],
+            );
+          } else {
+            insertDriverData();
+          }
+        },
+      );
+
+      // Create Jobs table
+      txn.executeSql(
+        'SELECT * FROM sqlite_master WHERE type="table" AND name="Jobs_Table"',
+        [],
+        function (tx, res) {
+          // console.log('Jobs_Table item:', res.rows.length);
+          // tx.executeSql(
+          //   'DROP TABLE IF EXISTS Jobs_Table',
+          //   [],
+          //   res => {
+          //     console.log('delete success');
+          //   },
+          //   error => {
+          //     console.log(error);
+          //   },
+          // );
+          if (res.rows.length == 0) {
+            txn.executeSql('DROP TABLE IF EXISTS Jobs_Table', []);
+            txn.executeSql(
+              'CREATE TABLE IF NOT EXISTS Jobs_Table(job_id INTEGER PRIMARY KEY AUTOINCREMENT,' +
+                'driver_id INT(5),' +
+                'job_reference VARCHAR(30),' +
+                'customer_name VARCHAR(30),' +
+                'customer_pin INT(4),' +
+                'job_address_one VARCHAR(255),' +
+                'job_address_one_type VARCHAR(255),' +
+                'outgoing_mass VARCHAR(10),' +
+                'outgoing BLOB,' +
+                'outgoing_bin_size VARCHAR(3),' +
+                'job_address_two VARCHAR(255),' +
+                'job_address_two_type VARCHAR(255),' +
+                'incoming_mass VARCHAR(10),' +
+                'incoming BLOB,' +
+                'incoming_bin_size VARCHAR(3),' +
+                'waste_id INT(3),' +
+                'customer_verification BOOLEAN,' +
+                'job_status VARCHAR(20))',
+              [],
+              res => {
+                insertJobsData();
+              },
+              error => {
+                console.log(error);
+              },
+            );
+          } else {
+            insertJobsData();
+          }
+        },
+      );
+
+      // Create Waste table
+      txn.executeSql('SELECT * FROM Waste_Table', [], function (tx, res) {
+        // console.log('Waste_Table item:', res.rows.length);
+        if (res.rows.length == 0) {
+          txn.executeSql('DROP TABLE IF EXISTS Waste_Table', []);
+          txn.executeSql(
+            'CREATE TABLE IF NOT EXISTS Waste_Table(waste_id INTEGER PRIMARY KEY AUTOINCREMENT,' +
+              'waste_type VARCHAR(30),',
+            [],
+          );
+        } else {
+          insertWasteData();
+        }
+      });
+    });
+  };
+
+  const insertDriverData = () => {
+    db.transaction(function (txn) {
+      txn.executeSql(
+        'SELECT driver_name FROM Driver_Table WHERE driver_name="kayllan"',
+        [],
+        function (tx, res) {
+          if (res.rows.length === 0) {
+            tx.executeSql(
+              'INSERT INTO Driver_Table (driver_name, driver_phone, driver_address, driver_pin) VALUES (?, ?, ?, ?)',
+              ['kayllan', '0765892981', 'drivers address', '1111'],
+              function (tx, res) {
+                // console.log('item:', res.rowsAffected);
+              },
+              function (error) {
+                console.log(error);
+              },
+            );
+          }
+        },
+        function (error) {
+          console.log(error);
+        },
+      );
+    });
+  };
+
+  const customers = [
+    {
+      name: 'Mercedes-Benz SA',
+      params: [
+        'Job#001',
+        'Mercedes-Benz SA',
+        'Cnr Hendrick Potgieter &, William Nicol Dr, Constantia Kloof, Johannesburg, 1709',
+        'Dropoff',
+        '1141 Leader Ave, Stormill, Roodepoort, 1725',
+        'Collect',
+        'Active',
+      ],
+    },
+    {
+      name: 'Mercedes-Benz SA',
+      params: [
+        'Job#002',
+        'cccc',
+        '42 Electron Ave, Isando, Kempton Park, 1600',
+        'Dropoff',
+        '42 Electron Ave, Isando, Kempton Park, 1600',
+        'Collect',
+        'Active',
+      ],
+    },
+    {
+      name: 'Nestle',
+      params: [
+        'Job#003',
+        'Nestle',
+        '8 Anslow Ln, Bryanston, Sandton, 2191',
+        'Dropoff',
+        '8 Anslow Ln, Bryanston, Sandton, 2191',
+        'Collect',
+        'Active',
+      ],
+    },
+    {
+      name: 'Feltex',
+      params: [
+        'Job#004',
+        'Feltex',
+        '41 Martinus Rass St, Rosslyn, Pretoria, 0200',
+        'Dropoff',
+        '41 Martinus Rass St, Rosslyn, Pretoria, 0200',
+        'Collect',
+        'Active',
+      ],
+    },
+  ];
+
+  const insertJobsData = () => {
+    db.transaction(function (txn) {
+      for (let customer of customers) {
+        txn.executeSql(
+          'SELECT customer_name FROM Jobs_Table WHERE customer_name="' +
+            customer.name +
+            '"',
+          [],
+          (tx, res) => {
+            console.log(res.rows.length);
+            if (res.rows.length === 0) {
+              tx.executeSql(
+                'INSERT INTO Jobs_Table (job_reference, customer_name, job_address_one, job_address_one_type, job_address_two, job_address_two_type, job_status) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                [customer.params],
+                rs => {
+                  console.log(rs);
+                },
+                error => {
+                  console.log(error);
+                },
+              );
+            }
+          },
+          error => {
+            console.log(error);
+          },
+        );
+      }
+      // txn.executeSql(
+      //   'INSERT INTO Jobs_Table (job_reference, customer_name, job_address_one, job_address_one_type, job_address_two, job_address_two_type, job_status) VALUES (?, ?, ?, ?, ?, ?, ?), (?, ?, ?, ?, ?, ?, ?), (?, ?, ?, ?, ?, ?, ?), (?, ?, ?, ?, ?, ?, ?)',
+      //   [
+      //     'Job#001',
+      //     'Mercedes-Benz SA',
+      //     'Cnr Hendrick Potgieter &, William Nicol Dr, Constantia Kloof, Johannesburg, 1709',
+      //     'Dropoff',
+      //     '1141 Leader Ave, Stormill, Roodepoort, 1725',
+      //     'Collect',
+      //     'Active',
+      //     'Job#002',
+      //     'cccc',
+      //     '42 Electron Ave, Isando, Kempton Park, 1600',
+      //     'Dropoff',
+      //     '42 Electron Ave, Isando, Kempton Park, 1600',
+      //     'Collect',
+      //     'Active',
+      //     'Job#003',
+      //     'Nestle',
+      //     '8 Anslow Ln, Bryanston, Sandton, 2191',
+      //     'Dropoff',
+      //     '8 Anslow Ln, Bryanston, Sandton, 2191',
+      //     'Collect',
+      //     'Active',
+      //     'Job#004',
+      //     'Feltex',
+      //     '41 Martinus Rass St, Rosslyn, Pretoria, 0200',
+      //     'Dropoff',
+      //     '41 Martinus Rass St, Rosslyn, Pretoria, 0200',
+      //     'Collect',
+      //     'Active',
+      //   ],
+      //   function (tx, res) {
+      //     console.log('item:', res.rowsAffected);
+      //   },
+      //   function (error) {
+      //     console.log(error);
+      //   },
+      // );
+    });
+  };
+
+  const insertWasteData = () => {
+    db.transaction(function (txn) {
+      txn.executeSql(
+        'INSERT INTO Waste_Table (waste_type) VALUES (?, ?, ?, ?, ?, ?)',
+        [
+          'General Waste',
+          'HL1 White Paper',
+          'HL2 Colour Paper',
+          'Pallets',
+          'Light Steel',
+          '25l Steel Drum',
+        ],
+        function (tx, res) {
+          // console.log('item:', res.rowsAffected);
+        },
+      );
+    });
+  };
+
+  useEffect(() => {
     if (count === 4) {
       if (pinCode.join('') === '1111') {
         navigation.navigate('Home');
@@ -57,7 +343,7 @@ const EnterPinView = props => {
 
   return (
     <SafeAreaView style={PinCodeContainer}>
-      {getLocation()}
+      {createDB()}
       <View>
         <View style={PinCodeHeaderContainer}>
           <Title style={{color: 'black'}}>Enter Your Pin</Title>
